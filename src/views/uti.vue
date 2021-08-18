dd9dc845b6553d306ee22daf1f32bc3ac70abab9
Mohit Darmal <mohitdarmal1907@gmail.com>
2021-08-18 14:41:08 +0530

    saving uti form data

--- src/views/uti.vue
+++ src/views/uti.vue
@@ -3,26 +3,26 @@
 
     <section class="section is-main-section">
       <card-component title="UTI">
-        <form @submit.prevent="submit">
-
+        <form  method="post" @submit.prevent="getUtiFormData">
+<!-- <form> -->
         <div class="columns">
             <div class="column is-one-third">
               <b-field label="When">
-                <b-input>
+                <b-input v-model="utiForm.when">
                 </b-input>
               </b-field>
             </div>
 
             <div class="column is-one-third">
-              <b-field label="Months">
-                <b-input>
+              <b-field label="Months" >
+                <b-input v-model="utiForm.months">
                 </b-input>
               </b-field>
             </div>
 
             <div class="column is-one-third">
               <b-field label="Days">
-                <b-input>
+                <b-input v-model="utiForm.days">
                 </b-input>
               </b-field>
             </div>
@@ -31,27 +31,27 @@
           <div class="columns">
             <div class="column is-one-third">
               <b-field label="Treated">
-                <b-input maxlength="300" type="textarea"></b-input>
+                <b-input maxlength="300" type="textarea" v-model="utiForm.treated"></b-input>
               </b-field>
             </div>
 
             <div class="column is-one-third">
               <b-field label="CUE">
-                <b-input maxlength="300" type="textarea"></b-input>
+                <b-input maxlength="300" type="textarea" v-model="utiForm.cue"></b-input>
               </b-field>
             </div>
 
             <div class="column is-one-third">
               <b-field label="What drugs">
-                <b-input maxlength="300" type="textarea"></b-input>
+                <b-input maxlength="300" type="textarea" v-model="utiForm.drugs"></b-input>
               </b-field>
             </div>
           </div>
 
 
 
-          <b-button type="sbmt-btn">Submit</b-button>
-
+          <button class="sbmt-btn" type="submit" >Submit</button>
+<!-- v-on:click="getUtiFormData" -->
         </form>
       </card-component>
 
@@ -67,6 +67,9 @@
   import RadioPicker from '@/components/RadioPicker'
   import FilePicker from '@/components/FilePicker'
   import HeroBar from '@/components/HeroBar'
+  import Axios from 'axios'
+  import VueAxios from 'vue-axios'
+
   export default {
     name: 'Forms',
     components: {
@@ -79,10 +82,17 @@
     },
     data() {
       return {
-        radio: 'default',
-         checkboxGroup: ['Flint'],
-        isLoading: false,
-        form: {
+        utiForm : {
+          when : null,
+          months : null,
+          days : null,
+          treated : null,
+          cue :  null,
+          drugs : null
+        },
+
+
+        /* form: {
           name: null,
           email: null,
           phone: null,
@@ -95,8 +105,8 @@
           radio: null,
           switch: true,
           file: null
-        },
-        departments: ['Business Development', 'Marketing', 'Sales']
+        }, */
+
       }
     },
     computed: {
@@ -105,7 +115,19 @@
       }
     },
     methods: {
-      submit() {},
+
+      getUtiFormData () {
+        // console.log("Data", this.utiForm);
+
+
+        Axios.post("", this.utiForm).then((response) => {
+  console.log(response.data)
+})
+
+
+      },
+
+      // submit() {},
       reset() {
         this.form = mapValues(this.form, (item) => {
           if (item && typeof item === 'object') {
